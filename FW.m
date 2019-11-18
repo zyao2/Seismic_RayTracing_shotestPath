213f6059952c6f721abde054f4bb565551a502e4
zyao2 <zhengshengy@yahoo.com>
2019-11-17 19:05:36 -0700

    Update FW.m

--- FW.m
+++ FW.m
@@ -1,80 +1,108 @@
 function [Model]=FW(m,n,s_i,s_j,N2In,In2N,zpos,xpos,Model) 
 global zorder xorder
-A=zeros(m*n,1);        
+Frozen=zeros(m,n); 
 %input source location 
 Model(s_i,s_j).time=0; 
 node_i=s_i; 
 node_j=s_j; 
-k=In2N(s_i,s_j);
-A(k)=1;                 %move in
-P(1)=N2In(s_i,s_j);     %move in
-na=1;
-
+Frozen(node_i,node_j)=1;
 %find number of node 
-counter_P=1;%nnz(P); % nnz -- Number of nonzero matrix elements.
 
-%find shortest time node
-while counter_P<(m*n) 
+Heap = zeros(3, m*n);
+heapCount = 0;
+x0=xpos(node_j);
+z0=zpos(node_i);
+t0=Model(node_i,node_j).time;
+nInd_b = In2N(node_i,node_j);
+[LV,V_i,V_j]=Fw_star(node_i,node_j,m,n,zorder,xorder); 
+for ii=1:LV 
+   i=V_i(ii); 
+   j=V_j(ii);
+   if(Model(i,j).time==-1)
+        heapCount = heapCount + 1;
+        heap_ind = heapCount;
+        Model(i,j).time=heap_ind;
+        nInd = In2N(i,j);
+        x=xpos(j);
+        z=zpos(i);
+        dist=(x-x0).^2+(z-z0).^2; 
+        dist=sqrt(dist);
+        slowness=2/(Model(i,j).velocity+Model(node_i,node_j).velocity); 
+        delta_t=dist*slowness;
+        tt=t0+delta_t;
+        Heap(1,heap_ind) = tt;
+        Heap(2,heap_ind) = nInd;
+        Heap(3,heap_ind) = nInd_b;
+   end
+end   
+
+while (heapCount>0)
+    [t0 heap_ind] = min(Heap(1,1:heapCount));
+    CP = Heap(2,heap_ind);
+    nInd_b = Heap(3,heap_ind);
+    node_i=N2In(CP).i;
+    node_j=N2In(CP).j;
+    Model(node_i,node_j).time=t0;
+    Model(node_i,node_j).node=nInd_b;
+    Frozen(node_i,node_j)=1;
     x0=xpos(node_j);
     z0=zpos(node_i);
-    %Determining all node sets V connected to the node where the 
-    %minial time node is located
+    v0=Model(node_i,node_j).velocity;
+
+    if (heap_ind ~= heapCount)
+		Heap(:,heap_ind) = Heap(:,heapCount);
+		% Also remember the pointer in T to the heap element
+        kk=Heap(2,heap_ind);
+        ii=N2In(kk).i;
+        jj=N2In(kk).j;
+		Model(ii,jj).time = heap_ind;     
+    end		
+	% Decrement heap counter
+	heapCount = heapCount - 1;
+   
     [LV,V_i,V_j]=Fw_star(node_i,node_j,m,n,zorder,xorder); 
     for ii=1:LV 
         i=V_i(ii); 
         j=V_j(ii);
-        k=In2N(i,j);
-        if(A(k)==0)
-            k=In2N(i,j);
-            A(k)=1;              %move in
-            na=na+1;
+        if (Frozen(i,j))
+            continue;
         end
-    end   
-    %Move the wavelet node out of the set Q and 
-    for j=1:counter_P
-        k=In2N(P(j).i,P(j).j);
-        if(A(k)==1)
-            A(k)=0;    %move out
-            na=na-1;
-        end
-    end
-    time_min=inf;
-    time_min_i=0;
-    time_min_j=0;
-    for ii=1:m*n    %na
-        if(A(ii)==1)
-            pp=N2In(ii);
-            i=pp.i; 
-            j=pp.j; 
+        if (Model(i,j).time==-1)
+			heapCount = heapCount + 1;
+			heap_ind = heapCount;
+            Model(i,j).time=heap_ind;
+			% Add pointer from heap to T
+            kk=In2N(i,j);
+            Heap(2,heap_ind) = kk;
+            Heap(3,heap_ind) = CP;
+			% Calculate tentative T(neigh) distance,
+			% and list it in the heap
             x=xpos(j);
             z=zpos(i);
-            dist=(x-x0).^2+(z-z0).^2; 
-            dist=sqrt(dist);
-            slowness=2/(Model(i,j).velocity+Model(node_i,node_j).velocity); 
-            %Model(i,j).uptime=dist*slowness; 
-            delta_t=dist*slowness;
-            time_try=Model(node_i,node_j).time+delta_t;%Model(i,j).uptime; 
-            if time_try<Model(i,j).time 
-                Model(i,j).time=time_try; 
-                Model(i,j).before_i=node_i; 
-                Model(i,j).before_j=node_j;
-                Model(i,j).dist=dist;
-            end
-            if(time_min>Model(i,j).time)
-                time_min=Model(i,j).time;
-                time_min_i=i;
-                time_min_j=j;
+            vv=Model(i,j).velocity;
+            ss=2/(vv+v0);
+                
+            dist=sqrt((x-x0)^2+(z-z0)^2);
+            tt=dist*ss+t0;
+            Heap(1,heap_ind) = tt;          
+        else
+			% Get heap indice of (i,j)
+			heap_ind = Model(i,j).time;					
+			% Calculate distance
+            x=xpos(j);
+            z=zpos(i);
+            vv=Model(i,j).velocity;
+            ss=2/(vv+v0);
+                
+            dist=sqrt((x-x0)^2+(z-z0)^2);
+            tt=dist*ss+t0;
+            if(tt<Heap(1,heap_ind))
+                Heap(1,heap_ind) = tt;
+                Heap(3,heap_ind) = CP;
             end
         end
-    end 
-    node_i=time_min_i;
-    node_j=time_min_j;
- 
-    %calculate the number of nodes in the P set
-    counter_P=counter_P+1;
-    pp.i=node_i;
-    pp.j=node_j;
-    P(counter_P)=pp;
+    end   
 end 
 disp('!===== Forward End =====!')  
 return;
+
