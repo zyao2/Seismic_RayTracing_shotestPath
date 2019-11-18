2cde9cbab98ad19b920b0739cc7b55463724fe1a
zyao2 <zhengshengy@yahoo.com>
2019-11-17 19:07:58 -0700

    Update Demo.m

--- Demo.m
+++ Demo.m
@@ -1,34 +1,35 @@
+close all
+clear all
 global zorder xorder
-xorder=4; zorder=4;
-Length=20; n=40;%j -- x
-Width=18;m=48; %i  --  z
-src_x = 1.0;  src_z = 2.0;         % Shot 
-rec_x = 1.0;  rec_z = 38.0;        % Recieve 
+zorder=7;xorder=7;
+n=100;%j -- x
+m=52; %i  --  z
+src_z = 1.0;  src_x = 90.0;         % Shot 
+rec_z = 1.0;  rec_x = 4.0;        % Recieve 
 VModel=zeros(m,n);
 xpos=zeros(m,1);
 zpos=zeros(n,1);
+dz=5;dx=5;
 for k=1:m
-    VModel(k,:)=2+2*k;
+    VModel(k,:)=800+44*k*dz;
 end
 
-for j=1:m
-    xpos(j)=(j-1)*Length/(n-1);
+for j=1:n
+    xpos(j)=(j-1)*5;
 end
 for i=1:m
-    zpos(i)=(i-1)*Width/(m-1);
+    zpos(i)=(i-1)*5;
 end
-Model=struct('velocity',zeros(m,n),'before_i',zeros(m,n),...
-    'before_j',zeros(m,n),'time',zeros(m,n));
+Model=struct('velocity',zeros(m,n),'node',zeros(m,n),...
+   'time',zeros(m,n),'dist',zeros(m,n));
 N2In=struct('i',zeros(m*n,1),'j',zeros(m*n,1));
 %initializing
 k=0;
 for i=1:m 
     for j=1:n 
-          k=k+1;
           Model(i,j).velocity=VModel(i,j);  %
-          Model(i,j).before_i=-1; 
-          Model(i,j).before_j=-1; 
-          Model(i,j).time=inf; 
+          Model(i,j).node=-1; 
+          Model(i,j).time=-1; 
     end 
 end 
 In2N=zeros(m,n);
@@ -41,11 +42,9 @@ for i=1:m
         In2N(i,j)=k;
     end
 end    
-
-Model = FW(m,n,src_x,src_z,N2In,In2N,zpos,xpos,Model);
-%% plot the result 
-[ray_x,ray_z]=getRay(Model,rec_x,rec_z,src_x,src_z);
-
+t = cputime;
+Model = FW(m,n,src_z,src_x,N2In,In2N,zpos,xpos,Model);
+[ray_x,ray_z]=getRay(Model,rec_z,rec_x,src_z,src_x,N2In);
 DotTime=zeros(m,n);
 for k1=1:m
     for k2=1:n
@@ -55,10 +54,13 @@ end
 figure;
 subplot(1,2,1)  
 contour(DotTime);hold on 
+
 grid; 
 subplot(1,2,2)  
 plot(ray_x,ray_z,m,n);     hold on 
-plot(src_x,src_z,'r*');hold on 
-plot(rec_x,rec_z,'rv',m,n);hold on 
+plot(src_z,src_x,'r*');hold on 
+plot(rec_z,rec_x,'rv',m,n);hold on 
 contour(VModel'); 
 grid 
+e = cputime-t
+
